37ab3c4f4d2aecc02c9a631a4e2c603843905e95
Atanas Atanasov <scriptex.bg@gmail.com>
2021-05-25 09:52:22 +0300

    Update dependencies (#351)

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 import babel from 'rollup-plugin-babel';
 import resolve from 'rollup-plugin-node-resolve';
 import commonJS from 'rollup-plugin-commonjs';
-import { uglify } from 'rollup-plugin-uglify';
+import { terser } from 'rollup-plugin-terser';
 
 export default {
 	input: 'src/scrollspy.js',
@@ -20,6 +20,6 @@ export default {
 		commonJS({
 			include: 'node_modules/**'
 		}),
-		uglify()
+		terser()
 	]
 };
